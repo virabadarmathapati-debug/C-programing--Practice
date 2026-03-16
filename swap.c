117a12d6cfc1ac76decaae744ecf8a1ada7d2ba1
Basavva V Mathapati <virabadarmathapati@gmail.com>
2026-03-16 15:12:05 +0530

    Adding program to perform swapping two numbers

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter  two numbers\n");
+scanf("%d""%d",&a,&b);
+{
+printf("before swaping a=%d,b=%d\n",a,b);
+int temp=a;
+a=b;
+b=temp;
+printf("after swaping a=%d&b=%d\n",a,b);
+}
+}
